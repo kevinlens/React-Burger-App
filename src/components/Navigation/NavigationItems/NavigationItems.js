8f9952446a84e226194c99097c70b872520fa0f9
kevin <kevinmt191919@gmail.com>
2020-04-21 20:54:57 -0500

    More Additions to Navigation Bar

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+import classes from './NavigationItems.module.css'
+
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = () => (
+// A <ul> is basically a <div> tag but reserved for, well, <ul>
+    <ul className={classes.NavigationItems}>
+
+        {/* the "Burger Build" and "Checkout" passed in will represent the 
+        "{props.children}" of <NavigationItem></NavigationItem> */}
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/" active >Checkout</NavigationItem>
+
+    </ul>
+);
+
+export default navigationItems;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,13 +4,15 @@ import classes from './Toolbar.module.css'
 
 import Logo from '../../Logo/Logo'
 
+import NavigationItems from '../NavigationItems/NavigationItems';
+
 const toolbar = (props) => (
     // A header is basically a <div> tag but reserved for, well, header
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 
