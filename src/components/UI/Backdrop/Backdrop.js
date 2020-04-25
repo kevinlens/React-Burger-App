2fe4c43a1df5fbe20c35a91d68d0a52b70794a86
KTran-Tech <kevinmt191919@gmail.com>
2020-04-24 23:49:28 -0500

    changes

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -4,6 +4,7 @@ import classes from './Backdrop.module.css';
 
 //Stateless component aka (BackgroundMenuCancel)
 const backdrop = (props) => (
+    // if show is true, meaning it exist then print out the element
     props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
 );
 
