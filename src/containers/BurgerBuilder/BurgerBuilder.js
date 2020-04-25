7d388bc3bcc3f98e051cac59e7ff3c5f8c6f102e
ktran-tech <kevinmt191919@gmail.com>
2020-04-25 12:48:49 -0500

    Major bug Fix

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -15,6 +15,8 @@ import Modal from '../../components/UI/Modal/Modal'
 //Importing STATELESS OrderSummary component from component file
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
     salad: 0.5,
     cheese: 0.4,
@@ -58,7 +60,26 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () =>{
-        alert('You continue!');
+        //alert('You continue!');
+        const order = {
+            ingredient: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Kevin Tran',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '23123',
+                    country: 'Germany'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response=>
+                console.log('response')
+            )
+            .catch(error => console.log(error))
     }
 
     //A Method to update on whether or not the cart functionality can work
