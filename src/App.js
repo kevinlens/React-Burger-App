c0c14bd6b72a195a12300d8a3f8dedf3306045db
ktran-tech <kevinmt191919@gmail.com>
2020-04-29 12:24:36 -0500

    Feature: New Page with Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
 
 //Importing in the LAYOUT Component from STATELESS component files
 import Layout from './hoc/Layout/Layout'
 //Importing in the BurgerBuilder Component from STATEFUL container files
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
+
+import Checkout from './containers/Checkout/Checkout'
 
 /*Using a class based component because it will be dealing with
 stateful component*/
@@ -16,7 +19,10 @@ class App extends Component {
 
         {/* App layout */}
         <Layout>
-          <BurgerBuilder />
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" component={BurgerBuilder} />
+        </Switch>
         </Layout>
 
 
