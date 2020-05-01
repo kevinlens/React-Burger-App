12b2c2615047b024006aea405a7099e31d2247f9
ktran-tech <kevinmt191919@gmail.com>
2020-04-30 22:33:21 -0500

    ErrorHandler added for Orders file

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -4,7 +4,7 @@ import Order from '../../components/Order/Order'
 
 import axios from '../../axios-orders'
 
-
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 class Orders extends Component {
 
@@ -47,4 +47,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
