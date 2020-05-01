1060f9babdc304f2f216bc230bb3e69e8fde6b8b
ktran-tech <kevinmt191919@gmail.com>
2020-04-30 22:29:25 -0500

    Data for Orders Page Section

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,7 +2,41 @@ import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order'
 
+import axios from '../../axios-orders'
+
+
+
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount(){
+        //base url is already set to: https://burger-app-project-b3079.firebaseio.com/
+        axios.get('./orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+
+                //the "data" an object containing additional objects returned
+                for(let key in res.data){
+                    fetchedOrders.push({
+                        //spread out the objects with specific data out of the main object
+                        ...res.data[key],
+                        id:key
+                    })
+                }
+                this.setState({
+                    orders: fetchedOrders,
+                    loading: false
+                })
+            })
+            .catch(err =>{
+                this.setState({loading: false})
+            });
+    }
+
     render() {
         return(
             <div>
