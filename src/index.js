59374d2b42f8251a45086184f3d7997413358791
KTran-Tech <kevinmt191919@gmail.com>
2020-05-05 17:36:51 -0500

    Redux Setup

--- src/index.js
+++ src/index.js
@@ -7,13 +7,20 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
 
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
+<Provider store={store}>
   <React.StrictMode>
-<BrowserRouter>
-    <App />
-</BrowserRouter>
-  </React.StrictMode>,
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+  </React.StrictMode>
+</Provider>,
   document.getElementById('root')
 );
 
